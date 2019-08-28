2c13f15f561ad8dfe4f2abf09852bc413efb7ac6
woosukjeung <47073569+woosukjeung@users.noreply.github.com>
2019-08-27 22:33:28 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, redirect
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 
 def get_fact():
-
     response = requests.get("http://unkno.com")
 
     soup = BeautifulSoup(response.content, "html.parser")
@@ -17,12 +16,29 @@ def get_fact():
     return facts[0].getText()
 
 
+def pig_latinate(string):
+    try:
+        request_url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+        data = {'input_text': string}
+        r = requests.post(request_url, data, allow_redirects=False)
+
+        soup = BeautifulSoup(r.text, "html.parser")
+        url = soup.find_all("a")[0].getText()
+
+        latinize_url = 'https://hidden-journey-62459.herokuapp.com' + url
+    except ConnectionError:
+        latinize_url = 'Connection failed, please retry. :('
+
+    return latinize_url
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact()
+    pig_latin_url = pig_latinate(fact)
+    return redirect(pig_latin_url, code=302)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
